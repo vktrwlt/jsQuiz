99684839ca9b40cbea02243dfdd9e1873c89c2fb
Victor Tsang <victorwltsang@gmail.com>
2016-07-23 15:11:11 -0700

    made random array

--- js/quiz.js
+++ js/quiz.js
@@ -1,54 +1,8 @@
-var arr = [];
-var len = 10;
-
-// generate random # from 1 to 16
-for (var i = 0; i < len; i++) {
-    arr.push(Math.floor((Math.random() * 15) + 1));
-}
-
-//need to refactor this code
-var a = arr[0];
-var b = arr[1];
-var c = arr[2];
-var d = arr[3];
-var e = arr[4];
-var f = arr[5];
-var g = arr[6];
-var h = arr[7];
-
-
-
-
-var quiz = [{
-        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x);",
-
-        "answer": "[" + [a, b, c] + "]"
-    }, {
-        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[0]);",
-
-        "answer": a
-    }, {
-        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[1]);",
-
-        "answer": b
-    }, {
-        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[2]);",
-
-        "answer": c
-    }, {
-        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> x[0] = x[1]; <br/> console.log(x);",
-
-        "answer": "[" + [b, b, c] + "]"
-    }
-
-
-
-];
 
 var currentQuestion = 0;
 var score = 0;
 var answer = document.getElementById("ans");
-
+var quiz = {};
 //Press Enter key to submit answer
 $("#ans").keypress(function(e) {
     if (e.which == 13) {
@@ -104,13 +58,20 @@ function tutorial() {
 
 
 function loadQuestion() {
+   quiz = [{
+          "question": "var x = 1; <br/> console.log(x);",
 
+          "answer": "1"
+      }];
     document.getElementById("question").innerHTML = quiz[currentQuestion]["question"];
 
 }
 
 
 function nextQuestion() {
+
+
+
     answer = document.getElementById("ans").value;
 
     //Check if input is the answer, if so +1 to score.
@@ -132,10 +93,62 @@ function nextQuestion() {
     if (currentQuestion == quiz.length) {
         currentQuestion = 0;
     }
-
-    loadQuestion();
-
+    randomQuestion();
+    document.getElementById("question").innerHTML = quiz[currentQuestion]["question"];
+    document.getElementById("sol").innerHTML = quiz[currentQuestion]["answer"];
 }
+
+function randomQuestion(){
+  var arr = [];
+  var len = 10;
+
+  // generate random # from 1 to 16
+  for (var i = 0; i < len; i++) {
+      arr.push(Math.floor((Math.random() * 15) + 1));
+  }
+
+  //need to refactor this code
+  var a = arr[0];
+  var b = arr[1];
+  var c = arr[2];
+  var d = arr[3];
+  var e = arr[4];
+  var f = arr[5];
+  var g = arr[6];
+  var h = arr[7];
+
+
+
+
+   quiz = [{
+          "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x);",
+
+          "answer": "[" + [a, b, c] + "]"
+      }, {
+          "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[0]);",
+
+          "answer": a
+      }, {
+          "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[1]);",
+
+          "answer": b
+      }, {
+          "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[2]);",
+
+          "answer": c
+      }, {
+          "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> x[0] = x[1]; <br/> console.log(x);",
+
+          "answer": "[" + [b, b, c] + "]"
+      }
+
+
+
+  ];
+}
+
+
+
 // insert code for popup and retry button
 function endGame() {
 
