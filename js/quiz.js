3cad1435dcfd7396c1e29ff5caee75cc7c6c0f1f
Victor Tsang <victorwltsang@gmail.com>
2016-07-23 13:14:07 -0700

    made it into a functional game

--- js/quiz.js
+++ js/quiz.js
@@ -1,10 +1,9 @@
-
 var arr = [];
 var len = 10;
 
 // generate random # from 1 to 16
-for (var i = 0; i < len; i++){
-	arr.push(Math.floor((Math.random() * 15) + 1));
+for (var i = 0; i < len; i++) {
+    arr.push(Math.floor((Math.random() * 15) + 1));
 }
 
 //need to refactor this code
@@ -20,32 +19,27 @@ var h = arr[7];
 
 
 
-var quiz = [
-  {
-    "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x);",
+var quiz = [{
+        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x);",
 
-    "answer": "[" + [a,b,c] + "]"
- },
-  {
-    "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[0]);",
+        "answer": "[" + [a, b, c] + "]"
+    }, {
+        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[0]);",
 
-    "answer": a
-  },
- {
-   "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[1]);",
+        "answer": a
+    }, {
+        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[1]);",
 
-   "answer": b
- },
- {
-   "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[2]);",
+        "answer": b
+    }, {
+        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> console.log(x[2]);",
 
-   "answer": c
- },
-{
-  "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> x[0] = x[1]; <br/> console.log(x);",
+        "answer": c
+    }, {
+        "question": "var x = [" + a + "," + b + "," + c + "];" + "<br/> x[0] = x[1]; <br/> console.log(x);",
 
-  "answer": "[" + [b,b,c] + "]"
-}
+        "answer": "[" + [b, b, c] + "]"
+    }
 
 
 
@@ -57,53 +51,98 @@ var answer = document.getElementById('ans');
 
 //Press Enter key to submit answer
 $('#ans').keypress(function(e) {
-if (e.which == 13) {
-nextQuestion();
-}
+    if (e.which == 13) {
+        nextQuestion();
+    }
 });
+
+function countdown(minutes) {
+    var seconds = 60;
+    var mins = minutes
+    function tick() {
+        var counter = document.getElementById("counter");
+        var current_minutes = mins-1
+        seconds--;
+        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+
+				//Open score & retry popup when counter is 0.
+				if(counter.innerHTML == "0:00"){
+					endGame();
+				}
+
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+
+        } else {
+
+            if(mins > 1){
+
+                countdown(mins-1);
+
+            }
+        }
+
+    }
+
+    tick();
+
+}
+
 
 
 //insert Tona's code here
-function tutorial(){
+function tutorial() {
 
-  //Remove this later
-  // alert("Start Game?");
+    //Remove this later
+    alert("Start Game?");
 
-  //load Questions
-  loadQuestion();
+		//countdown begins when alert or modal is done.
+		countdown(1);
+    //load Questions
+    loadQuestion();
 }
 
 
-function loadQuestion(){
 
-document.getElementById('question').innerHTML = quiz[currentQuestion]["question"];
+function loadQuestion() {
 
-}
-
-
-function nextQuestion(){
-   answer = document.getElementById('ans').value;
-
-  //Check if input is the answer, if so +1 to score.
-  if (answer.toString().trim() == quiz[currentQuestion]["answer"].toString()){
-    score++;
-  }
-  //reset the answer bar
-  document.getElementById('ans').value = "";
-
-  //reset the notepad
-  document.getElementById('pad').value = "";
-  
-  // test score counter
-  document.getElementById('test').textContent = score;
-
-  currentQuestion++;
-
-  // Remove this later, lazy code for having only 4 questions
-  if(currentQuestion == quiz.length){
-    currentQuestion = 0;
-  }
-  loadQuestion();
-
+    document.getElementById('question').innerHTML = quiz[currentQuestion]["question"];
 
 }
+
+
+function nextQuestion() {
+    answer = document.getElementById('ans').value;
+
+    //Check if input is the answer, if so +1 to score.
+    if (answer.toString().trim() == quiz[currentQuestion]["answer"].toString()) {
+        score++;
+    }
+    //reset the answer bar
+    document.getElementById('ans').value = "";
+
+    //reset the notepad
+    document.getElementById('pad').value = "";
+
+    // test score counter
+    document.getElementById('test').textContent = score;
+
+    currentQuestion++;
+
+    // Remove this later, lazy code for having only 4 questions
+    if (currentQuestion == quiz.length) {
+        currentQuestion = 0;
+    }
+    loadQuestion();
+
+}
+ // insert code for popup and retry button
+ function endGame(){
+
+	 alert("GG! You scored:  " + score);
+	 var retry = confirm("Retry");
+	 if(retry){
+		 location.reload();
+	 }
+
+ }
